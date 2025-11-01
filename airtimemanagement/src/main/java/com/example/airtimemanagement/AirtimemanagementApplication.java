4171d95b211690d138dcf072ad2306e6da08c8dd
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2025-11-01 19:52:02 +0300

    Initial Project Setup

---
+++ airtimemanagement/src/main/java/com/example/airtimemanagement/AirtimemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.airtimemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirtimemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirtimemanagementApplication.class, args);
+	}
+
+}
